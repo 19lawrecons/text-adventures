94aeecfddf733f5452cc883a209d304678e6ec58
Merit Student <student@meac.local>
2018-09-12 13:30:50 -0600

    notes

---
+++ something.py
@@ -0,0 +1,11 @@
+print("To start, type \'start\'")
+fromUser = input()
+while True:
+    if fromUser == "start":
+        print("Start game!")
+    elif fromUser == "quit":
+        print("Game over!")
+        break
+    else:
+        print("To start, type 'start'")
+        fromUser = input()
